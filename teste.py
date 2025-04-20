f75ef9fabd1ac572a5da4960a74fd3e38ed1bd4c
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-04-20 11:43:41 -0300

    atualização

--- teste.py
+++ teste.py
@@ -5,10 +5,13 @@ data_hora_atual = pendulum.now('America/Sao_Paulo').to_iso8601_string()
 data_hora_atual = pendulum.parse(data_hora_atual)
 hora_atual = int(data_hora_atual.hour)
 data = data_hora_atual.format('YYYY_MM_DD')
-data_hora_busca = data_hora_atual.subtract(hours=7)
-data_hora_busca = data_hora_busca.strftime('%Y-%m-%dT%H:%M:%SZ')
+# data_hora_busca = data_hora_atual.subtract(hours=7)
+data_hora_busca = data_hora_atual.strftime('%Y-%m-%dT%H:%M:%SZ')
 print(data_hora_busca)
 ay = ApiYoutube()
 
-ay.obter_assunto(assunto='Python', data_publicacao_apos=data_hora_busca)
+dados = ay.obter_assunto(assunto='Python', data_publicacao_apos=data_hora_busca)
+
+for dado in dados:
+    print(dado)
 
