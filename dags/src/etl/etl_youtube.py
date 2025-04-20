8dd5d82a6764e61ac7d66f0a581206856751717a
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-04-20 12:39:48 -0300

    testes

--- dags/src/etl/etl_youtube.py
+++ dags/src/etl/etl_youtube.py
@@ -6,6 +6,22 @@ class ETLYoutube:
         self.__api_youtube = api_youtube
         self.__arquivo_hadoop = None
 
-    def processo_etl_assunto_video(self, assunto: str, data_publicacao_apos: str):
+    def processo_etl_assunto_video(
+            self,
+            assunto: str,
+            data_publicacao_apos: str,
+            data_pesquisa='2025-04-01T00:00:00Z'
+    ):
         for response in self.__api_youtube.obter_assunto(assunto=assunto, data_publicacao_apos=data_publicacao_apos):
             print(response)
+            response['data_pesquisa'] = data_pesquisa
+            response['assunto'] = assunto
+            print(response)
+            print(response['id']['videoId'], response['snippet']['title'])
+            print(response['snippet']['channelId'], response['snippet']['channelTitle'])
+
+
+if __name__ == '__main__':
+    from dags.src.services.apiyoutube.api_youtube import ApiYoutube
+
+    etl_youtube = ETLYoutube(api_youtube=ApiYoutube())
