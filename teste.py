32bd17cc868a1f12ff1eea01a8f3a30efee71450
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-04-21 17:21:00 -0300

    git ignore

--- teste.py
+++ teste.py
@@ -6,12 +6,26 @@ data_hora_atual = pendulum.parse(data_hora_atual)
 hora_atual = int(data_hora_atual.hour)
 data = data_hora_atual.format('YYYY_MM_DD')
 # data_hora_busca = data_hora_atual.subtract(hours=7)
+
 data_hora_busca = data_hora_atual.strftime('%Y-%m-%dT%H:%M:%SZ')
-print(data_hora_busca)
-ay = ApiYoutube()
 
-dados = ay.obter_assunto(assunto='Python', data_publicacao_apos=data_hora_busca)
+from dateutil import parser
 
-for dado in dados:
-    print(dado)
+data_str = '2025-04-21T00:00:00Z'
+data_dt = parser.isoparse(data_str)
+print(type(data_dt))
+print(data_dt.strftime('%A'))
+
+
+dias_semana = {
+    0: 'Segunda-feira',
+    1: 'Terça-feira',
+    2: 'Quarta-feira',
+    3: 'Quinta-feira',
+    4: 'Sexta-feira',
+    5: 'Sábado',
+    6: 'Domingo'
+}
+nome_dia_pt = dias_semana[data_dt.weekday()]
+print(nome_dia_pt)
 
