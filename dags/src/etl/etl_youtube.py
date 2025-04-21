a284e4a8fcc05f69fea76713a5db0cbe38757a87
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-04-21 13:40:59 -0300

    tipagem do método executar consulta para tupla

--- dags/src/etl/etl_youtube.py
+++ dags/src/etl/etl_youtube.py
@@ -1,7 +1,7 @@
 from dags.src.services.apiyoutube.i_api_youtube import IApiYoutube
 from dags.src.services.manipulacao_dados.ioperacao_banco import IOperacaoBanco
 from datetime import datetime
-
+from dateutil import parser
 
 class ETLYoutube:
     def __init__(self, api_youtube: IApiYoutube, operacoes_banco: IOperacaoBanco):
@@ -30,22 +30,46 @@ class ETLYoutube:
             data_publicacao_apos: str,
             data_pesquisa='2025-04-01T00:00:00Z'
     ):
-        # Criar partição
-        for response in self.__api_youtube.obter_assunto(assunto=assunto, data_publicacao_apos=data_publicacao_apos):
-            print(response)
-            response['data_pesquisa'] = data_pesquisa
-            response['assunto'] = assunto
-            print(response)
-            print(response['id']['videoId'], response['snippet']['title'])
-            print(response['snippet']['channelId'], response['snippet']['channelTitle'])
-            dados_canais = self.__api_youtube.obter_dados_canais(id_canal=response['snippet']['channelId'])
-            # Gravar dados Vídeos
-            if dados_canais[1] == 'BR':
-                dados_canais[0]['data_pesquisa'] = data_pesquisa
-                dados_canais[0]['assunto'] = assunto
-                # Gravar dados canais
-                print(dados_canais[0])
-            break
+        data = parser.isoparse(data_pesquisa)
+        ano = data.year
+        mes = data.month
+        dia = data.day
+        dia_semana = self.__obter_semana_portugues(data=data)
+
+        consulta = f"""
+                ALTER TABLE bronze_assunto
+                ADD IF NOT EXISTS PARTITION (
+                    ano={ano},
+                    mes={mes},
+                    dia={dia},
+                    dia_semana='{dia_semana}',
+                    assunto="{assunto}"
+                );
+                """
+        dados = self.__operacoes_banco.executar_consulta(consulta=consulta)
+
+        if dados[0]:
+
+            # Criar partição
+            for response in self.__api_youtube.obter_assunto(assunto=assunto, data_publicacao_apos=data_publicacao_apos):
+                print(response)
+                response['data_pesquisa'] = data_pesquisa
+                response['assunto'] = assunto
+                print(response)
+                print(response['id']['videoId'], response['snippet']['title'])
+                print(response['snippet']['channelId'], response['snippet']['channelTitle'])
+                dados_canais = self.__api_youtube.obter_dados_canais(id_canal=response['snippet']['channelId'])
+                # Gravar dados Vídeos
+                if dados_canais[1] == 'BR':
+                    dados_canais[0]['data_pesquisa'] = data_pesquisa
+                    dados_canais[0]['assunto'] = assunto
+                    # Gravar dados canais
+                    print(dados_canais[0])
+                break
+        else:
+            # Tratamento de erro
+            # parar a rotina
+            pass
 
 
 if __name__ == '__main__':
