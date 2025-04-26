02ae591aa58f8d43f87bec409f3ad02dcc000a93
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-04-26 17:35:39 -0300

    módulo com o recursos do hive

---
+++ dags/src/services/manipulacao_dados/operacao_banco_hive_airlow.py
@@ -0,0 +1,24 @@
+try:
+    import sys
+    import os
+
+    sys.path.insert(0, os.path.abspath(os.curdir))
+except ModuleNotFoundError:
+    pass
+from typing import Tuple, Any
+from airflow.providers.apache.hive.hooks.hive import HiveServer2Hook
+from dags.src.services.manipulacao_dados.ioperacao_dados import IOperacaoDados
+
+
+class OperacaoBancoHiveAirflow(IOperacaoDados):
+    def __init__(self):
+        self.__id = 'id_hive'
+        self.__hook = HiveServer2Hook(hiveserver2_conn_id=self.__id)
+
+    def executar_consulta_dados(self, consulta: str) -> Tuple[bool, Any]:
+        try:
+            result = self.__hook.get_records(consulta)
+            return True, result
+        except Exception as e:
+            print(e)
+            return False, None
