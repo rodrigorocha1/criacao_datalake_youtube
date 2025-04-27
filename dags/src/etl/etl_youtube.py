cccd5b7695fccb7234ca2bcac3196715009dcc80
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-04-27 12:46:55 -0300

    Validando inserção assunto

--- dags/src/etl/etl_youtube.py
+++ dags/src/etl/etl_youtube.py
@@ -52,75 +52,35 @@ class ETLYoutube:
 
         return nome_dia
 
+    def __fazer_tratamento_assunto(self, assunto: str) -> str:
+        assunto = assunto.replace("'", "").replace(' ', '_')
+        return assunto
+
+    def __preparar_caminho_particao(self, termo_pesquisa: str, nome_arquivo: str, assunto_tratado: str):
+        self.__operacoes_arquivo.camada = 'bronze'
+        self.__operacoes_arquivo.termo_pesquisa = termo_pesquisa
+        self.__operacoes_arquivo.caminho_particao = (
+            f'ano={self.__ano}'
+            f'/mes={self.__mes}'
+            f'/dia={self.__dia}'
+            f'/dia_semana={self.__dia_semana.replace(" ", "_")}'
+            f'/assunto={assunto_tratado}'
+        )
+        self.__operacoes_arquivo.nome_arquivo = nome_arquivo
+
     def __criar_particao(self, tabela_particao: str):
         consulta = f"""
                     ALTER TABLE {tabela_particao}
-                    ADD IF NOT EXISTS PARTITION (
+                    ADD IF NOT EXISTS PARTITION ( 
                         ano={self.__ano},
                         mes={self.__mes},
-                        dia={self.__dia_semana},
+                        dia={self.__dia},
                         dia_semana='{self.__dia_semana.replace(' ', '_')}',
                         assunto="{unidecode(self.__assunto).replace(' ', '_').replace("'", "")}"
                 )
                 """
         dados = self.__operacoes_banco.executar_consulta_dados(consulta=consulta)
 
-    def processo_etl_assunto_video(self, data_publicacao_apos: str):
-        termo_pesquisa = self.__fazer_tratamento_assunto(assunto=self.__assunto)
-        self.__preparar_caminho_particao(termo_pesquisa=termo_pesquisa, nome_arquivo='asssunto.json')
-        self.__criar_particao(tabela_particao='bronze_assunto')
-        for response in self.__api_youtube.obter_assunto(
-                assunto=self.__assunto,
-                data_publicacao_apos=data_publicacao_apos
-        ):
-            response['data_pesquisa'] = data_publicacao_apos
-            response['assunto'] = self.__assunto
-            self.__operacoes_arquivo.guardar_dados(dado=response)
-            dados_canais = self.__api_youtube.obter_dados_canais(id_canal=response['snippet']['channelId'])
-            if dados_canais[1] == 'BR':
-                dados_canais[0]['data_pesquisa'] = data_publicacao_apos
-                dados_canais[0]['assunto'] = self.__assunto
-                id_canal = response['snippet']['channelId']
-                nome_canal = response['snippet']['channelTitle']
-                print('Canal Brasileiro', id_canal)
-                print('Video Brasilero')
-
-                self.__inserir_dados_novos(
-                    assunto=termo_pesquisa,
-                    tabela='canais',
-                    valor_insercao=(id_canal, nome_canal),
-                    coluna_verificacao='id_canal',
-                    valor_verificacao=id_canal,
-
-                )
-
-                id_video = response['id']['videoId']
-                titulo_video = response['snippet']['title']
-
-                self.__inserir_dados_novos(
-                    assunto=termo_pesquisa,
-                    tabela='videos',
-                    valor_insercao=(id_video, titulo_video),
-                    coluna_verificacao='id_video',
-                    valor_verificacao=id_video
-
-                )
-
-    def __fazer_tratamento_assunto(self, assunto: str) -> str:
-        assunto = assunto.replace("'", "").replace('', '_')
-        return assunto
-
-    def __preparar_caminho_particao(self, termo_pesquisa: str, nome_arquivo: str):
-        self.__operacoes_arquivo.camada = 'bronze'
-        self.__operacoes_arquivo.termo_pesquisa = termo_pesquisa
-        self.__operacoes_arquivo.caminho_particao = (
-            f'ano={self.__ano}'
-            f'/mes={self.__mes}'
-            f'/dia={self.__dia_semana}'
-            f'/dia_semana={self.__dia_semana.replace(" ", "_")}'
-            f'/assunto={unidecode(self.__assunto).replace(" ", "_")}'
-        )
-        self.__operacoes_arquivo.nome_arquivo = nome_arquivo
 
     def __inserir_dados_novos(
             self,
@@ -149,9 +109,58 @@ class ETLYoutube:
                 """
             consulta_canal = self.__operacoes_banco.executar_consulta_dados(consulta=consulta)
 
+    def processo_etl_assunto_video(self, data_publicacao_apos: str):
+        assunto_tratado = self.__fazer_tratamento_assunto(assunto=self.__assunto)
+        self.__preparar_caminho_particao(
+            termo_pesquisa='assunto',
+            nome_arquivo='asssunto.json',
+            assunto_tratado=assunto_tratado
+        )
+        self.__criar_particao(tabela_particao='bronze_assunto')
+        for response in self.__api_youtube.obter_assunto(
+                assunto=self.__assunto,
+                data_publicacao_apos=data_publicacao_apos
+        ):
+            response['data_pesquisa'] = data_publicacao_apos
+            response['assunto'] = self.__assunto
+            self.__operacoes_arquivo.guardar_dados(dado=response)
+            dados_canais = self.__api_youtube.obter_dados_canais(id_canal=response['snippet']['channelId'])
+            if dados_canais[1] == 'BR':
+                dados_canais[0]['data_pesquisa'] = data_publicacao_apos
+                dados_canais[0]['assunto'] = self.__assunto
+                id_canal = response['snippet']['channelId']
+                nome_canal = response['snippet']['channelTitle']
+                print('Canal Brasileiro', id_canal)
+                print('Video Brasilero')
+
+                self.__inserir_dados_novos(
+                    assunto=assunto_tratado,
+                    tabela='canais',
+                    valor_insercao=(id_canal, nome_canal),
+                    coluna_verificacao='id_canal',
+                    valor_verificacao=id_canal,
+
+                )
+
+                id_video = response['id']['videoId']
+                titulo_video = response['snippet']['title']
+
+                self.__inserir_dados_novos(
+                    assunto=assunto_tratado,
+                    tabela='videos',
+                    valor_insercao=(id_video, titulo_video),
+                    coluna_verificacao='id_video',
+                    valor_verificacao=id_video
+
+                )
+
     def processo_etl_canal(self, assunto: str, data_pesquisa: str = '2025-04-01T00:00:00Z'):
-        termo_pesquisa = self.__fazer_tratamento_assunto(assunto=self.__assunto)
-        self.__preparar_caminho_particao(termo_pesquisa=termo_pesquisa, nome_arquivo='canal.json')
+        assunto_tratado = self.__fazer_tratamento_assunto(assunto=self.__assunto)
+        self.__preparar_caminho_particao(
+            termo_pesquisa='canal',
+            nome_arquivo='canal.json',
+            assunto_tratado=assunto_tratado
+        )
 
         consulta = f"""
             SELECT *
@@ -179,8 +188,12 @@ class ETLYoutube:
             # tratamento de erro
 
     def processo_etl_video(self, assunto: str, data_pesquisa: str = '2025-04-01T00:00:00Z'):
-        termo_pesquisa = self.__fazer_tratamento_assunto(assunto=self.__assunto)
-        self.__preparar_caminho_particao(termo_pesquisa=termo_pesquisa, nome_arquivo='video.json')
+        assunto_tratado = self.__fazer_tratamento_assunto(assunto=self.__assunto)
+        self.__preparar_caminho_particao(
+            termo_pesquisa='video',
+            nome_arquivo='video.json',
+            assunto_tratado=assunto_tratado
+        )
 
         consulta = f"""
                     SELECT *
