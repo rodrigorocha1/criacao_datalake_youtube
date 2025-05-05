2fdff93a66e4db84a13b19977066ec13ff3f05f3
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-05-04 22:18:23 -0300

    ajuste enum

--- dags/enums/camada_enum.py
+++ dags/enums/camada_enum.py
@@ -1,7 +1,19 @@
 from enum import Enum
 
+
 class Camada(Enum):
     Bronze = 'bronze'
-    Prata  = 'prata'
+    Prata = 'prata'
     Ouro = 'ouro'
-    Depara = 'Depara'
+    Depara = 'depara'
+
+
+if __name__ == "__main__":
+    a = Camada.Bronze.value
+    print(type(Camada.Bronze.value))
+    print(a)
+
+    def teste(c: Camada):
+        print(c)
+
+    teste(Camada.Depara.value)
