f9c9895f80a358a6ddf6e132eab2822f2568c9e4
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-05-10 14:40:22 -0300

    atualização hook

--- dags/src/operatorss/youtube_busca_operator.py
+++ dags/src/operatorss/youtube_busca_operator.py
@@ -1,5 +1,5 @@
 
-from dags.src.operators.youtube_operator import YoutubeOperator
+from dags.src.operatorss.youtube_operator import YoutubeOperator
 from hook.youtube_hook import YotubeHook
 from src.services.manipulacao_dados.arquivo_json import ArquivoJson
 from typing import Dict
@@ -24,28 +24,8 @@ class YoutubeBuscaOperator(YoutubeOperator):
         )
 
     def gravar_dados(self, req: Dict):
-
         req['assunto'] = self._assunto
-
         self.__arquivo_json.guardar_dados(dado=req)
-
-        lista_canal_video = [
-            (
-                item['snippet']['channelId'],
-                item['id']['videoId']
-            )
-            for item in req['items']
-        ]
-
-        lista_canais = [
-            item['snippet']['channelId']
-            for item in req['items']
-        ]
-
-        self._arquivo_pkl_canal.salvar_dados(dados=lista_canais)
-
-        if self._arquivo_pkl_canal_video is not None:
-            self._arquivo_pkl_canal_video.salvar_dados(dados=lista_canal_video)
 
     def execute(self, context):
         try:
