c2b704514716b57570fdead128dc637ae6885fb6
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-05-10 13:55:50 -0300

    Atualização hook

---
+++ dags/src/hook/youtube_busca_assunto_hook.py
@@ -0,0 +1,42 @@
+from hook.youtube_hook import YotubeHook
+
+
+class YoutubeBuscaAssuntoHook(YotubeHook):
+
+    def __init__(self,  data_publicacao: str, assunto_pesquisa=str, conn_id=None):
+        self.__data_publicacao = data_publicacao
+        self.__assunto_pesquisa = assunto_pesquisa
+        super().__init__(conn_id=conn_id)
+
+    def _criar_url(self) -> str:
+        """Retorna a url
+
+        Returns:
+            str: _description_
+        """
+        return self._URL + '/search/'
+
+    def run(self):
+        """Método para rodar a dag
+
+        Returns:
+            _type_: _description_
+        """
+        session = self.get_conn()
+        url = self._criar_url()
+        params = [
+            {
+                'part':  'snippet',
+                'key': self._CHAVE,
+                'regionCode': 'BR',
+                'relevanceLanguage': 'pt',
+                'maxResults': '50',
+                'publishedAfter': self.__data_publicacao,
+                'q': self.__assunto_pesquisa,
+                'pageToken': ''
+            }
+        ]
+
+        response = self._executar_paginacao(
+            url=url, session=session, params=params)
+        return response
---
+++ dags/src/hook/youtube_hook.py
@@ -0,0 +1,86 @@
+from datetime import datetime
+from typing import Iterable, List, Optional, Dict
+from abc import ABC, abstractmethod
+from airflow.providers.http.hooks.http import HttpHook
+import requests
+from src.configuracao.configuracao import Configuracao
+
+
+class YotubeHook(HttpHook, ABC):
+
+    def __init__(self, conn_id: str) -> None:
+        """Método para inicializar o youtube hook
+
+        Args:
+            conn_id (str, optional): id do airflow. Defaults to None.
+            carregar_dados (IInfraDados, optional): tipo de carregamento de dados. Defaults to None.
+        """
+        self._conn_id = conn_id
+        self._URL = Configuracao.url
+        self._CHAVE = Configuracao.chave
+
+        super().__init__(http_conn_id=self._conn_id)
+
+    @abstractmethod
+    def _criar_url(self):
+        pass
+
+    def _executar_paginacao(self, url: str, session, params: List[Dict]) -> Iterable[Dict]:
+        """Gerador para obter requisição da api do youtube
+
+        Args:
+            url (str): url da api
+            session (_type_): session airflow
+            params (List[Dict]): lista de parâmetos requeridos pela api
+
+        Returns:
+            Iterable[Dict]: a requisição da api
+
+        Yields:
+            Iterator[Iterable[Dict]]: um json com as requisições da api
+        """
+        i = 1
+        next_token = ''
+        for param in params:
+            while next_token is not None:
+                response = self.conectar_api(url, param, session)
+                if response:
+                    json_response = response.json()
+                    json_response['data_pesquisa'] = datetime.now().strftime(
+                        '%Y-%m-%d %H:%M:%S'
+                    )
+
+                    yield json_response
+                    try:
+                        next_token = json_response['nextPageToken']
+                        param['pageToken'] = next_token
+                        print('próximo token', next_token)
+                    except KeyError:
+                        break
+
+                else:
+                    break
+
+    def conectar_api(self, url: str, params: Dict, session) -> Optional[requests.models.Response]:
+        """Método para conectar na api
+
+        Args:
+            url (str): url escolhida
+            params (Dict): params da api
+            session (_type_): session do airflow
+
+        Returns:
+            _type_: a conexão
+        """
+        try:
+            response = requests.Request('GET', url=url, params=params)
+
+            prep = session.prepare_request(response)
+
+            return self.run_and_check(session, prep, {})
+        except:
+            return None
+
+    @abstractmethod
+    def run(self):
+        pass
