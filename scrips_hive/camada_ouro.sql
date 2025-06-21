739fceb811b1039e409bdb1acbc2b5fa26dc6096
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-06-21 16:34:47 -0300

    prototipo consulta com o tempo

--- scrips_hive/camada_ouro.sql
+++ scrips_hive/camada_ouro.sql
@@ -2,21 +2,44 @@
 --- Total visualizacoes_dia selecionando canal
 
 select 
-	pc.nm_canal as nome_canal,
-	pc.semana as semana,
-	pc.mes as mes,
-
 	pc.total_visualizacoes as total_visualizacoes_acumuladas,
-	LAG(pc.total_visualizacoes, 1, 0) OVER (PARTITION BY pc.id_canal ORDER BY pc.mes) AS visualizacoes_acumuladas_anterior,
-	pc.total_visualizacoes - LAG(pc.total_visualizacoes, 1, 0) OVER (PARTITION BY pc.id_canal ORDER BY pc.mes) as TOTAL_VISUALIZACAO_DIA
+	LAG(pc.total_visualizacoes, 1, 0) OVER (PARTITION BY pc.id_canal ORDER BY pc.mes, pc.dia ) AS visualizacoes_acumuladas_anterior,
+	pc.total_visualizacoes - LAG(pc.total_visualizacoes, 1, 0) OVER (PARTITION BY pc.id_canal ORDER BY pc.mes, pc.dia) as TOTAL_VISUALIZACAO_DIA,
+	pc.nm_canal as nome_canal,
+	DATE_FORMAT(CONCAT_WS('-', CAST(pc.ano AS STRING), LPAD(CAST(pc.mes AS STRING), 2, '0'), LPAD(CAST(pc.dia AS STRING), 2, '0')), 'yyyy-MM-dd') dia_formatado,
+	pc.semana as semana,
+	pc.assunto as assunto,
+	pc.ano as ano,
+	pc.mes as mes,
+	pc.dia as dia,
+	case when pc.semana == 'Domingo'
+		then 0
+	when pc.semana == 'Segunda'
+		then 1
+	when pc.semana == 'Terça'	
+		then 2
+	when pc.semana == 'Quarta'	
+		then 3
+	when pc.semana == 'Quinta'	
+		then 4
+	when pc.semana == 'Sexta'	
+		then 5
+	when pc.semana == 'Sábado'	
+		then 6 end as indice_semana,
+    pc.id_canal as id_canal
 from prata_canal pc 
-where pc.id_canal = 'UCpcCgSD1BEDiEQ8EkY7Comw'
-order by pc.mes ;
+ -- where pc.id_canal = 'UCGxmd2AnLNrSWFhrCcEj0lQ'  
+-- AND  pc.dia BETWEEN 24 and 25  and pc.mes in (5, 6)
+ where DATE_FORMAT(CONCAT_WS('-', CAST(pc.ano AS STRING), LPAD(CAST(pc.mes AS STRING), 2, '0'), LPAD(CAST(pc.dia AS STRING), 2, '0')), 'yyyy-MM-dd') >= CURRENT_DATE() - 1
+order by pc.id_canal , pc.mes, pc.dia ;
+
+
+SELECT CURRENT_DATE() - 1;
 
 
 select *
 from prata_canal pc 
-where pc.id_canal = 'UCpcCgSD1BEDiEQ8EkY7Comw';
+where pc.id_canal = 'UCGxmd2AnLNrSWFhrCcEj0lQ';
 
 
 SELECT
@@ -47,8 +70,8 @@ select
 	LAG(pc.total_videos_publicados, 1, 0) OVER (PARTITION BY pc.id_canal ORDER BY pc.mes) AS videos_acumuladas_anterior,
 	pc.total_videos_publicados - LAG(pc.total_videos_publicados, 1, 0) OVER (PARTITION BY pc.id_canal ORDER BY pc.mes) as TOTAL_VIDEOS_PUBLICADOS
 from prata_canal pc 
-where pc.id_canal = 'UCpcCgSD1BEDiEQ8EkY7Comw'
-order by pc.mes ;
+-- where pc.id_canal = 'UCpcCgSD1BEDiEQ8EkY7Comw'
+order by pc.id_canal ,pc.mes ;
 
 
 
@@ -64,8 +87,8 @@ select
 	LAG(pc.total_inscritos, 1, 0) OVER (PARTITION BY pc.id_canal ORDER BY pc.mes) AS total_inscritos_anterior,
 	pc.total_inscritos - LAG(pc.total_inscritos, 1, 0) OVER (PARTITION BY pc.id_canal ORDER BY pc.mes) as total_inscritos_PUBLICADOS
 from prata_canal pc 
-where pc.id_canal = 'UCpcCgSD1BEDiEQ8EkY7Comw'
-order by pc.mes ;
+--- where pc.id_canal = 'UCpcCgSD1BEDiEQ8EkY7Comw'
+order by pc.id_canal ,pc.mes ;
 
 
 
@@ -87,5 +110,21 @@ from prata_video pv
 order by id_video
 
 
+select *
+from bronze_canais bc 
+
+
+show partitions bronze_videos
+
+
+ALTER TABLE bronze_videos  DROP IF EXISTS PARTITION (ano=2025, mes=5, dia=24, dia_semana='Sabado', assunto='no_mans_sky');
+ALTER TABLE bronze_videos DROP IF EXISTS PARTITION (ano=2025, mes=5, dia=24, dia_semana='Sabado', assunto='palworld');
+ALTER TABLE bronze_videos DROP IF EXISTS PARTITION (ano=2025, mes=5, dia=24, dia_semana='Sabado', assunto='python');
+ALTER TABLE bronze_videos DROP IF EXISTS PARTITION (ano=2025, mes=5, dia=25, dia_semana='Domingo', assunto='no_mans_sky');
+ALTER TABLE bronze_videos DROP IF EXISTS PARTITION (ano=2025, mes=5, dia=25, dia_semana='Domingo', assunto='palworld');
+ALTER TABLE bronze_videos DROP IF EXISTS PARTITION (ano=2025, mes=5, dia=25, dia_semana='Domingo', assunto='python');
+ALTER TABLE bronze_videos DROP IF EXISTS PARTITION (ano=2025, mes=6, dia=1, dia_semana='Domingo', assunto='no_mans_sky');
+ALTER TABLE bronze_videos DROP IF EXISTS PARTITION (ano=2025, mes=6, dia=1, dia_semana='Domingo', assunto='palworld');
+ALTER TABLE bronze_videos DROP IF EXISTS PARTITION (ano=2025, mes=6, dia=1, dia_semana='Domingo', assunto='python');
 
 
